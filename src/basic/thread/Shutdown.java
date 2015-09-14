c4d8184d98dfc6f3a0942ca429080fb25b4554ce
hcy <chengyuan.hu888@gmail.com>
2015-09-14 16:36:38 +0800

    add thread shut down sample from 《java并发编程的艺术》

---
+++ src/basic/thread/Shutdown.java
@@ -0,0 +1,35 @@
+package basic.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class Shutdown {
+	public static void main(String[] args) throws InterruptedException {
+		Runner one = new Runner();
+		Thread countThread = new Thread(one,"CountThread");
+		countThread.start();
+		//睡眠1秒，main线程对countThread进行中断，使countThread能够感知中断而结束
+		TimeUnit.SECONDS.sleep(3);
+		countThread.interrupt();
+		Runner two = new Runner();
+		countThread = new Thread(two,"CountThread");
+		countThread.start();
+		TimeUnit.SECONDS.sleep(3);
+		two.cancel();
+		
+	}
+	private static class Runner implements Runnable{
+		private long i;
+		private volatile boolean on  = true;
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(on&&Thread.currentThread().isInterrupted()){
+				i++;
+			}
+			System.out.println("Count i = "+i);
+		}
+		public void cancel(){
+			on = false;
+		}
+	}
+}
