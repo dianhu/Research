ce8665ac4725186d9d02058b83deeb24498c2961
hcy <chengyuan.hu888@gmail.com>
2015-09-16 09:40:58 +0800

    add

---
+++ src/basic/thread/Synchronized.java
@@ -0,0 +1,12 @@
+package basic.thread;
+
+public class Synchronized {
+	public static void main(String[] args) {
+		synchronized(Synchronized.class){
+			
+		}
+	}
+	public static synchronized void m(){
+		
+	}
+}
