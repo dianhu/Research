e4030d25dcf6eac57a4ae429a99169f00b3dacd0
hcy <chengyuan.hu888@gmail.com>
2016-09-08 15:49:17 +0800

    更新线程join的demo

--- src/basic/thread/simple/JoinDemo.java
+++ src/basic/thread/simple/JoinDemo.java
@@ -17,23 +17,32 @@ public class JoinDemo {
 				System.out.println("First task completed");
 			}
 		});
-		
-		Thread t2 = new Thread(new Runnable() {
-			public void run() {
-				System.out.println("Second task started");
-				try {
-					TimeUnit.SECONDS.sleep(1);
-					System.out.println("Second task sleeping for 1 seconds");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println("Second task completed");
-			}
-		});
+		Thread t2 = new Thread(new Mytask(t1));//等t1执行完了再执行t2
 		t1.start();
 		t2.start();
-		t1.join();//t1线程join到main线程中，main线程进入wait状态，等t1线程执行完毕，才被唤醒继续执行接下来的代码。
+		t1.join();// t1线程join到main线程中，main线程进入wait状态，等t1线程执行完毕，main线程才被唤醒继续执行接下来的代码。
 		System.out.println("main thread say hello!");
-		
+
+	}
+
+	static class Mytask implements Runnable {
+		private Thread dependThread;
+
+		public Mytask(Thread depend) {
+			this.dependThread = depend;
+		}
+
+		public void run() {
+			System.out.println("Second task started");
+			try {
+				this.dependThread.join();//等dependThread执行完了，再唤醒当前线程执行下面的语句。
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println("Second task sleeping for 1 seconds");
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("Second task completed");
+		}
 	}
 }
