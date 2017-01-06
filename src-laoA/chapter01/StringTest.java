8ac86a7441ad055efe54ab616748d5a2eb312679
hcy <chengyuan.hu888@gmail.com>
2017-01-06 18:10:11 +0800

    更新字符串比较的注释

--- src-laoA/chapter01/StringTest.java
+++ src-laoA/chapter01/StringTest.java
@@ -8,16 +8,16 @@ public class StringTest {
 	private static void test1(){
 		String a  = "a"+"b"+1;
 		String b = "ab1";
-		println(a==b);
+		println(a==b);//直接从常量池中取，为ture
 	}
 	
 	private static String getA(){return "a";}
 	private static void test2(){
 		String a = "a";
 		final String c = "a";
-		String b = a + "b";
-		String d = c + "b";
-		String e = getA()+"b";
+		String b = a + "b";//字符串变量的连接动作，在编译阶段会被转化成StringBuilder的append操作，变量b最终指向Java堆上新建String对象，变量compare指向常量池的"ab"字符串，所以b!=compare。
+		String d = c + "b";//加final的字符串变量会优化，还是从常量池中取，所以为true
+		String e = getA()+"b";//同字符串变量连接
 		String compare = "ab";
 		println(b==compare);
 		println(d==compare);
