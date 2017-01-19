697d916d94ab7faa57f6a14290903b1e33e90a31
hcy <chengyuan.hu888@gmail.com>
2017-01-19 15:19:22 +0800

    新增socket的一些简单示例

---
+++ src-algorithms/hcy/graphs/common/Graph.java
@@ -0,0 +1,13 @@
+package hcy.graphs.common;
+
+/**
+ * Time : 17-1-12 上午10:40
+ * Author : hcy
+ * Description :
+ */
+public class Graph {
+    /**
+     * 顶点
+     */
+    public Vertex[] vertices;
+}
---
+++ src-algorithms/hcy/graphs/common/Vertex.java
@@ -0,0 +1,17 @@
+package hcy.graphs.common;
+
+/**
+ * Time : 17-1-12 上午10:43
+ * Author : hcy
+ * Description :顶点
+ */
+public class Vertex {
+    /**
+     * 顶点名字
+     */
+    public String name;
+    /**
+     * 顶点一维数组中的下标
+     */
+    public int index;
+}
