98a5d137f92f15f0e271f4df4fa9ecca4608a2af
hcy <chengyuan.hu888@gmail.com>
2017-03-20 14:21:22 +0800

    增加常量池的demo

---
+++ src/basic/grammer/testStatic/StringTest1.java
@@ -0,0 +1,15 @@
+package basic.grammer.testStatic;
+
+/**
+ * Time : 17-3-20 下午1:33
+ * Author : hcy
+ * Description :
+ */
+public class StringTest1 {
+    public static String a = "hello";
+    public static int b = 10000;
+    public static float c = 11.5f;
+    public Integer d1=127;
+    public Integer d2=128;
+    public int d3= 128;
+}
